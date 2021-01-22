daa03e64f38f24b9d2fb848690e59cb2a4cc3ca6
Devichand1 <devichandinikhiya@gmail.com>
2021-01-22 08:38:08 +0530

    devi

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-const express = require('express')
-const app = express()
-const port = 5000
-
-app.post('/', function (req, res) {
-  res.send('Got a POST request')
-})
+const express = require('express');
+const port = process.env.PORT || 3000;
+const app = express();
+app.get('/', function (req, res) {
+ res.send(JSON.stringify({ Hello: 'world'}));
+});
+app.listen(port, function () {
+ console.log('Example app listening on port !');
+});
 
